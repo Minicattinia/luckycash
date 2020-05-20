74b71677a0ccbac0d04cd9c37615b66e7a9278b4
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2020-05-20 13:21:02 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+				 /*config by Anonymous24*/
+			/* Telegram @TeamAnonymous24 */
+
+$accesstoken ="x-access-token:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZGZhIjoiMTJlNzAyOTMtN2JlNC00ZTYyLWI3OTgtYWUxMTUxOWQzMTMwIiwicmFuZG9tU2VlZCI6MC42MjI1MTE4NzMwOTUwMDYzLCJpYXQiOjE1ODk5NTQyMjgsImV4cCI6MTU5MjU0NjIyOH0.k2ZuyP6Hazv1M2E75_fL2c7yoM0jv5Ik2kUTAkfRWr8";
+
+$userid ='5ec4c6967ee42e662b40b446';
+
+?>
